c2f4646bf6125d37140be2bb4fc63b749d06447a
Shion Ichikawa <shion1305@gmail.com>
2022-01-25 13:13:12 +0900

    Compatible with Discord4j ver3.2

--- src/main/java/com/shion1305/ynu_discord/tokuchan/TokuChanDiscordManager.java
+++ src/main/java/com/shion1305/ynu_discord/tokuchan/TokuChanDiscordManager.java
@@ -2,6 +2,9 @@ package com.shion1305.ynu_discord.tokuchan;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.Activity;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
 import discord4j.core.object.presence.Presence;
 import discord4j.discordjson.json.ActivityUpdateRequest;
 import discord4j.gateway.intent.IntentSet;
@@ -20,7 +23,7 @@ class TokuChanDiscordManager {
             clients = new HashMap<>();
         }
         if (clients.containsKey(token)) return clients.get(token);
-        GatewayDiscordClient client = DiscordClient.create(token).gateway().setEnabledIntents(IntentSet.all()).setInitialPresence(s -> Presence.online(ActivityUpdateRequest.builder().type(0).name("!introで使い方を確認! メッセージはDMで送信してね!").url("https://cdn.discordapp.com/avatars/898900972426915850/4b09f00b8b78094e931641a85077bcc3.png?size=512").build())).login().block();
+        GatewayDiscordClient client = DiscordClient.create(token).gateway().setEnabledIntents(IntentSet.all()).setInitialPresence(s -> ClientPresence.online(ClientActivity.of(Activity.Type.COMPETING, "!introで使い方を確認! メッセージはDMで送信してね!", "https://cdn.discordapp.com/avatars/898900972426915850/4b09f00b8b78094e931641a85077bcc3.png?size=512"))).login().block();
         clients.put(token, client);
         return client;
     }
