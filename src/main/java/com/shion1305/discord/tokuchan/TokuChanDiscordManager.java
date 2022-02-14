8acab64374d53f00bff5ecbb91fff570e507e1fa
Shion Ichikawa <shion1305@gmail.com>
2022-02-14 21:45:54 +0900

    Added Description in TokuChanDiscordManager.java

--- src/main/java/com/shion1305/discord/tokuchan/TokuChanDiscordManager.java
+++ src/main/java/com/shion1305/discord/tokuchan/TokuChanDiscordManager.java
@@ -10,10 +10,13 @@ import discord4j.gateway.intent.IntentSet;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+/**
+ * Discordのキーを安全かつ効率的に管理するためのクラス
+ * TokuChanHanlerインスタンスを呼び出す。
+ */
 class TokuChanDiscordManager {
     private static HashMap<String, GatewayDiscordClient> clients;
     private static final Logger logger = Logger.getLogger("TokuChanDiscordManager");
-
     static GatewayDiscordClient getClient(String token) {
         logger.info("DiscordClient requested: " + token.substring(token.length() - 10));
         if (clients == null) {
