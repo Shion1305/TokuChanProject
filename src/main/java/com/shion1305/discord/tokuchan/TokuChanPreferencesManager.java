8ff3185c83811dc041c9fa63ae2e7e4d62b4cbdc
Shion Ichikawa <shion1305@gmail.com>
2022-02-14 23:05:12 +0900

    Created TokuChanPreferencesManager.java that exclusively manages Preferences

---
+++ src/main/java/com/shion1305/discord/tokuchan/TokuChanPreferencesManager.java
@@ -0,0 +1,35 @@
+﻿package com.shion1305.discord.tokuchan;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.prefs.Preferences;
+
+class TokuChanPreferencesManager {
+    private static final Preferences prefs;
+
+    /* Preferenceを占有的に管理するクラス
+     */
+    static {
+        prefs = Preferences.userNodeForPackage(TokuChanPreferencesManager.class);
+    }
+
+    static Preferences getInstancePreference(String groupId) {
+        return prefs.node("Instances").node(groupId);
+    }
+
+    static HashMap<Long, User> readUserdata(long groupId) {
+        try (ObjectInputStream stream = new ObjectInputStream(new ByteArrayInputStream(getInstancePreference(String.valueOf(groupId)).getByteArray("UserData", null)))) {
+            return (HashMap<Long, User>) stream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void importOldData() {
+
+    }
+
+
+}
