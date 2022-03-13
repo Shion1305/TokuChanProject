43873e883592cc2c25ea88d1179214d481659e72
Shion Ichikawa <shion1305@gmail.com>
2022-03-14 03:14:57 +0900

    [OPTIMIZE] remove code for migration as the problem has been solved

--- src/main/java/com/shion1305/discord/tokuchan/instance/TokuChanPreferencesManager.java
+++ src/main/java/com/shion1305/discord/tokuchan/instance/TokuChanPreferencesManager.java
@@ -1,6 +1,5 @@
 package com.shion1305.discord.tokuchan.instance;
 
-import com.shion1305.discord.tokuchan.UserConverter;
 import com.shion1305.discord.tokuchan.instance.model.User;
 
 import java.io.*;
@@ -27,18 +26,7 @@ class TokuChanPreferencesManager {
         byte[] bData = getInstancePreference(groupId).getByteArray("userdata", null);
         if (bData != null) {
             try (ObjectInputStream stream = new ObjectInputStream(new ByteArrayInputStream(bData))) {
-                try {
-                    return (HashMap<Long, User>) stream.readObject();
-                } catch (ClassNotFoundException e) {
-                    HashMap<Long, com.shion1305.discord.tokuchan.User> tmp = (HashMap<Long, com.shion1305.discord.tokuchan.User>) stream.readObject();
-                    HashMap<Long, User> newData = new HashMap<>();
-                    for (Map.Entry<Long, com.shion1305.discord.tokuchan.User> entry : tmp.entrySet()) {
-                        newData.put(entry.getKey(), UserConverter.convert(entry.getValue()));
-                    }
-                    //migrate to new Data
-                    saveData(groupId, newData);
-                    return newData;
-                }
+                return (HashMap<Long, User>) stream.readObject();
             } catch (Exception e) {
                 e.printStackTrace();
             }
