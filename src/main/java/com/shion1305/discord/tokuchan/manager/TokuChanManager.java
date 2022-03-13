8b7094615cd19cd7aac358ad91a3ef1240017822
Shion Ichikawa <shion1305@gmail.com>
2022-03-14 01:44:19 +0900

    [IMPROVE] separated codes for loading into multiple methods in TokuChanManager.java

--- src/main/java/com/shion1305/discord/tokuchan/manager/TokuChanManager.java
+++ src/main/java/com/shion1305/discord/tokuchan/manager/TokuChanManager.java
@@ -21,33 +21,51 @@ public class TokuChanManager implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        load();
+    }
+
+    protected static void load() {
+        TokuChanData data = readTokuChanData();
+        if (data == null) {
+            logger.severe("JsonConfig\"" + ConfigManager.getConfig("TokuChanConfigJson") + "\" is not loaded properly");
+            return;
+        }
+        for (InstanceData instanceData : data.getInstances()) {
+            if (!instanceData.isValid()) {
+                logger.info("Skipped loading InstanceData\"" + instanceData.getName() + "\" as it is invalid...");
+                continue;
+            }
+            TokuChanInstance instance = new TokuChanInstance(instanceData);
+            instances.add(instance);
+        }
+    }
+
+    protected static TokuChanData readTokuChanData() {
         try {
             ObjectMapper mapper = new ObjectMapper();
             logger.info(ConfigManager.getConfig("TokuChanConfigJson"));
-            TokuChanData data = mapper.readValue(new File(ConfigManager.getConfig("TokuChanConfigJson")), TokuChanData.class);
-            if (data == null) {
-                logger.severe("JsonConfig\"" + ConfigManager.getConfig("TokuChanConfigJson") + "\" is not loaded properly");
-                return;
-            }
-            for (InstanceData instanceData : data.getInstances()) {
-                if (!instanceData.isValid()) {
-                    logger.info("Skipped loading InstanceData\"" + instanceData.getName() + "\" as it is invalid...");
-                    continue;
-                }
-                TokuChanInstance instance = new TokuChanInstance(instanceData);
-                instances.add(instance);
-            }
+            return mapper.readValue(new File(ConfigManager.getConfig("TokuChanConfigJson")), TokuChanData.class);
         } catch (IOException e) {
-            logger.severe("ERROR OCCURRED DURING LOADING JSON_CONFIG");
             e.printStackTrace();
         }
+        return null;
+    }
+
+    public static void reload() {
+        stop();
+        load();
+    }
+
+    protected static void stop() {
+        for (TokuChanInstance i : instances) {
+            i.stop();
+        }
+        instances.clear();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        for (TokuChanInstance i : instances) {
-            i.stop();
-        }
+        stop();
     }
 }
 
